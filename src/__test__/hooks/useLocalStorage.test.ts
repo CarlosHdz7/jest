186d3f14cfe23c1edd10836c05d5bbdd56c8461b
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-26 23:56:59 -0600

    ✨ Add test to useLocalStorage

---
+++ src/__test__/hooks/useLocalStorage.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jest/expect-expect */
+// eslint-disable-next-line import/no-unresolved
+import { renderHook, act } from '@testing-library/react-hooks';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
+describe('testing useLocalStorage', () => {
+  test('get default value localStorage', () => {
+    const { result } = renderHook(() => useLocalStorage('test', []));
+    const [state] = result.current;
+    expect(state).toEqual([]);
+  });
+
+  test('get value localStorage', () => {
+    localStorage.setItem('test', 'Hello world');
+    const { result } = renderHook(() => useLocalStorage('test', 'Hello world'));
+    const [state] = result.current;
+    expect(state).toEqual('Hello world');
+  });
+
+  test('set value localStorage', () => {
+    const { result } = renderHook(() => useLocalStorage('test', 'Hello world'));
+    const [, setState] = result.current;
+    act(() => {
+      setState('Hello test');
+    });
+    const [state2] = result.current;
+    expect(state2).toEqual('Hello test');
+  });
+});
