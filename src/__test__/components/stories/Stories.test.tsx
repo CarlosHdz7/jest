382cf13c393547b6bbd1cf8227bdc425022b5a75
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-31 00:12:07 -0600

    ✅ Add test to stories

--- src/__test__/components/stories/Stories.test.tsx
+++ src/__test__/components/stories/Stories.test.tsx
@@ -2,22 +2,60 @@
 /* eslint-disable react/react-in-jsx-scope */
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
+import * as reactRedux from 'react-redux';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router';
 import store from '../../../redux/store';
 import Stories from '../../../components/pages/stories';
+import storiesMock from '../../mocks/stories';
 
 describe('testing comics detail', () => {
   it('should show loader', () => {
-    const history = createMemoryHistory({ initialEntries: ['/stories/8'] });
-    const { container } = render(
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((callback) => callback({
+        stories: {
+          list: [],
+          loading: true,
+          bookmarks: [],
+        },
+      }));
+
+    const history = createMemoryHistory({ initialEntries: ['/stories'] });
+
+    const { queryByTestId } = render(
       <Provider store={store}>
         <Router history={history}>
           <Stories />
         </Router>
       </Provider>,
     );
-    expect(container.getElementsByClassName('loader')[0]).toBeInTheDocument();
+
+    expect(queryByTestId(/loader/i)).toBeTruthy();
+  });
+
+  it('should show characters list', () => {
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((callback) => callback({
+        stories: {
+          list: storiesMock,
+          loading: false,
+          bookmarks: [],
+        },
+      }));
+
+    const history = createMemoryHistory({ initialEntries: ['/stories'] });
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Stories />
+        </Router>
+      </Provider>,
+    );
+
+    expect(getByText('Investigating the murder of a teenage girl, Cage suddenly learns that a three-way gang war is under way for control of the turf')).toBeInTheDocument();
   });
 });
