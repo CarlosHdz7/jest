83dfa9d992bd26c567a47faa522003e5c018dc64
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-31 00:07:22 -0600

    ✅ Add test to comics list

--- src/__test__/components/comics/Comics.test.tsx
+++ src/__test__/components/comics/Comics.test.tsx
@@ -2,22 +2,93 @@
 /* eslint-disable react/react-in-jsx-scope */
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import * as reactRedux from 'react-redux';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router';
 import store from '../../../redux/store';
 import Comics from '../../../components/pages/comics';
+import { comicsMock } from '../../mocks/comics';
+
+type TestElement = Document | Element | Window | Node
+
+function hasInputValue(e: TestElement, inputValue: string) {
+  return screen.getByDisplayValue(inputValue) === e;
+}
 
 describe('testing comics detail', () => {
   it('should show loader', () => {
-    const history = createMemoryHistory({ initialEntries: ['/comics/1308'] });
-    const { container } = render(
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((callback) => callback({
+        comics: {
+          list: [],
+          loading: true,
+          bookmarks: [],
+        },
+      }));
+
+    const history = createMemoryHistory({ initialEntries: ['/comics'] });
+
+    const { queryByTestId } = render(
       <Provider store={store}>
         <Router history={history}>
           <Comics />
         </Router>
       </Provider>,
     );
-    expect(container.getElementsByClassName('loader')[0]).toBeInTheDocument();
+
+    expect(queryByTestId(/loader/i)).toBeTruthy();
+  });
+
+  it('should show comics list', () => {
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((callback) => callback({
+        comics: {
+          list: comicsMock,
+          loading: false,
+          bookmarks: [],
+        },
+      }));
+
+    const history = createMemoryHistory({ initialEntries: ['/comics'] });
+
+    const { getAllByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Comics />
+        </Router>
+      </Provider>,
+    );
+
+    expect(getAllByText('Ant-Man (2003) #4')[0]).toBeInTheDocument();
+    expect(getAllByText('Gun Theory (2003) #3')[0]).toBeInTheDocument();
+  });
+
+  it('should update search text', async () => {
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((callback) => callback({
+        comics: {
+          list: comicsMock,
+          loading: false,
+          bookmarks: [],
+        },
+      }));
+
+    const history = createMemoryHistory({ initialEntries: ['/comics'] });
+
+    const { getByRole } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Comics />
+        </Router>
+      </Provider>,
+    );
+
+    const input = getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'Spider' } });
+    expect(hasInputValue(input, 'Spider')).toBe(true);
   });
 });
